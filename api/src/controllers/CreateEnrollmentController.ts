16afec13d75ffdabe209bf505695b5a85131f0ba
douglas85rj <douglas85rj@gmail.com>
2023-06-27 21:36:47 -0300

    Validation_enrollment_ifexists

--- api/src/controllers/CreateEnrollmentController.ts
+++ api/src/controllers/CreateEnrollmentController.ts
@@ -5,13 +5,44 @@ export class CreateEnrollmentController {
   async handle(request: Request, response: Response) {
     const { alunoId, cursoId } = request.body;
 
+    const aluno = await prismaClient.aluno.findUnique({
+      where: {
+        id: alunoId,
+      },
+    });
+
+    if (!aluno) {
+      return response.status(400).json({ error: "Aluno não existe" });
+    }
+
+    const curso = await prismaClient.curso.findUnique({
+      where: {
+        id: cursoId,
+      },
+    });
+
+    if (!curso) {
+      return response.status(400).json({ error: "Curso não existe" });
+    }
+
+    const inscricaoJaExiste = await prismaClient.inscricao.findFirst({
+      where: {
+        alunoId,
+        cursoId,
+      },
+    });
+
+    if (inscricaoJaExiste) {
+      return response.status(400).json({ error: "Inscrição já existe" });
+    }
+
     const inscricao = await prismaClient.inscricao.create({
       data: {
         alunoId,
         cursoId,
         inscrito : true,       
       },
-       
+          
     });
 
 
